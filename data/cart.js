a4b309cbf40ad6c8e457bed0a1a63e536022dd75
Abbas Riaz <abbasriaz100@gmail.com>
2025-10-15 15:58:50 +0500

    make cart interactive

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,17 @@
+// import { products } from "./products.js";
 
-export let cart = [];
+export let cart = [
+  {
+    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 5
+  },
+
+
+  {
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+  }
+];
 
 export function addToCart(productId) {
   let matchingItem;
@@ -19,6 +31,8 @@ export function addToCart(productId) {
   }
 
 }
+
+
 
 
 

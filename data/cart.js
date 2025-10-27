74c63d9f97b69db37abf95a3d1eee544658bdef7
Abbas Riaz <abbasriaz100@gmail.com>
2025-10-27 09:21:44 +0500

    make cart rdate time interactive

--- data/cart.js
+++ data/cart.js
@@ -1,16 +1,16 @@
 
 export let cart = JSON.parse(localStorage.getItem('cart'));
-if (!cart) {
+if (cart.length == 0) {
 
   cart = [{
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity: 5,
-    deliveryOptionId: '1',
+    deliveryOptionId: '2',
   },
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
-    deliveryOptionId: '2',
+    deliveryOptionId: '3',
   }];
 
   console.log('null cart');
@@ -63,7 +63,19 @@ export function saveStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
 }
 
+export function updateDeliveryOptions(productId, deliveryOptionId) {
 
+  let matchingItem;
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
 
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
+  saveStorage();
+
+}
 
 
--- data/deliveryOption.js
+++ data/deliveryOption.js
@@ -1,4 +1,4 @@
- export const deliveryOptions= [
+export const deliveryOptions = [
   {
     id: '1',
     deliveryDays: 7,
@@ -10,7 +10,7 @@
     deliveryDays: 3,
     priceCents: 499,
   }, {
-    id: '2',
+    id: '3',
     deliveryDays: 1,
     priceCents: 999,
   }
